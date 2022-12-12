26b24507faac4a6c1d3d805950144de815c81799
cxandromeda <cxandromeda@gmail.com>
2022-12-11 19:13:13 -0500

    use two ways to show single user

--- server.js
+++ server.js
@@ -20,14 +20,40 @@ const data = [
     },
 ];
 
+
 // This route gets *ALL* the users
-app.get("/api/users", (req, res) => {
+// app.get("/api/users/all", (req, res) => {
+//     res.json(data);
+// });
+
+app.get("/api/users/", (req, res) => {
     res.json(data);
 });
+
 
 // Add a new route to get a *SINGLE* user (you can use either path param or query param)
 // /api/users/1      <-- path param (req.params.id)
 // /api/users?id=1   <-- query param (req.query.id)
+
+//below is to use path param
+
+app.get("/api/users/:id", (req, res) => {
+
+    let id = req.params.id;
+    // console.log(id);
+    res.json(data[id-1]);
+});
+
+//below is to use query param
+// app.get("/api/users", (req, res) => {
+
+//     let id = req.query.id;
+//     console.log(id);
+//     res.json(data[id-1]);
+// });
+
+
+
 
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
 
